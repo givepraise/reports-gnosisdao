f03f0faa777e5f1576b732c0baf07adc3d04b151
Kristofer <kristofer@fmckl.se>
2023-08-10 22:41:44 +0200

    Custom settings for GnosisDAO

--- src/index.js
+++ src/index.js
@@ -30,6 +30,8 @@ async function run() {
     console.log(`✅ Success!\n`);
     console.log(`Returned rows: ${response.rows.length}\n`);
 
+    console.log("Report log:", report.logData);
+
     console.log(`${JSON.stringify(response.rows, null, 2)}\n`);
   } catch (e) {
     console.log("Error generating report:");
