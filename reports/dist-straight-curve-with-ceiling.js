935daf16309c7183640e1f98d3f3de80a0e59ec2
Kristofer <kristofer@fmckl.se>
2023-08-10 22:56:08 +0200

    Debugging

--- reports/dist-straight-curve-with-ceiling.js
+++ reports/dist-straight-curve-with-ceiling.js
@@ -47,6 +47,7 @@ export default class DistStraightCurveWithCeiling extends BaseReport {
    */
   filterReceivers(rows) {
     const { filterReceivers } = this.config;
+    console.log(this.config);
     if (Array.isArray(filterReceivers) && filterReceivers.length > 0) {
       this.log(
         `\nFiltering ${filterReceivers.length} receivers based on setting "filterReceivers":`
